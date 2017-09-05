b67b218d08d975abe584f19913018ac3d9f67d70
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-09-05 22:26:47 +0200

    ensure that folder for external plugins exists before download

--- files/etc/my_init.d/10_gocd_plugins.sh
+++ files/etc/my_init.d/10_gocd_plugins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
+mkdir -p /var/lib/go-server/plugins/external
 curl -o /var/lib/go-server/plugins/external/git-fb-poller-1.3.4.jar https://github.com/ashwanthkumar/gocd-build-github-pull-requests/releases/download/v1.3.4/git-fb-poller-1.3.4.jar
